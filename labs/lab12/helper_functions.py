1d86827ee256cf1777d9fc47a416899d0f1dd2ce
Jacob Dentes <jdentes10@gmail.com>
2025-04-21 19:28:41 -0400

    Updated lab12 helper to use PuLP

--- labs/lab12/helper_functions.py
+++ labs/lab12/helper_functions.py
@@ -1,39 +1,34 @@
-def simplex_tableau(lp):
-    from tabulate import tabulate
-    import numpy as np
-    from IPython.display import display, Markdown
-    from ortools.linear_solver import pywraplp
+from tabulate import tabulate
+import numpy as np
+from IPython.display import display, Markdown
+from pulp import *
 
+def simplex_tableau(lp):
     # gets the variables of the model
     lp_vars = lp.variables()
-    num_basic = lp.NumConstraints()
+    num_basic = len(lp.constraints)
     # checks reduced costs to find basic variables
-    basic_vars = [k for k in lp_vars if round(k.ReducedCost(),5) == 0][:num_basic]
+    basic_vars = sorted([k for k in lp_vars], key=lambda x: abs(x.to_dict()['dj']))[:num_basic]
+    # basic_vars = [k for k in lp_vars if round(k.to_dict()['dj'],5) == 0][:num_basic]
+    def get_coeff(cons, var):
+        d = cons[1]
+        for v, val in d.items():
+            if v.to_dict()['name'] == var.to_dict()['name']:
+                return val
+        return 0
     # A is the matrix with the coefficients of the constraints
-    A = np.array([[c.GetCoefficient(i) for i in lp_vars] for c in lp.constraints()])
+    A = np.array([[get_coeff(c, i) for i in lp_vars] for c in lp.constraints.items()])
     # B is A limited on columns of basic variables
-    B = A[:,[k for k in range(len(lp_vars)) if lp_vars[k].name() 
-        in [i.name() for i in basic_vars]]]
-    bar_A =  np.round_(np.dot(np.linalg.inv(B),A),5)
-    temp = [[basic_vars[i].name()]+bar_A.tolist()[i]+[round(basic_vars[i].solution_value(),5)] 
+    basic_varnames = set([i.to_dict()['name'] for i in basic_vars])
+    B = A[:,[k for k in range(len(lp_vars)) if lp_vars[k].to_dict()['name'] in basic_varnames]]
+    bar_A =  np.round(np.dot(np.linalg.inv(B),A),5)
+    temp = [[basic_vars[i].to_dict()['name']]+bar_A.tolist()[i]+[round(value(basic_vars[i]),5)] 
             for i in range(len(basic_vars))]
     table = tabulate([
         ['Reduced costs $\\bar{c}_j$']
-            + [round(v.ReducedCost(),5) for v in lp_vars]
+            + [round(v.to_dict()['dj'],5) for v in lp_vars]
             + ['$\\bar{b}_i$']]+temp, 
        headers=(['']+lp_vars+['']), tablefmt='github')
     display(Markdown(table))
-    return (bar_A, [(i.name(),round(i.solution_value(),5)) for i in basic_vars],
-           [int(str(k)[1:]) for k in lp_vars if round(k.ReducedCost(),5) != 0])
-
-def reset(lp):
-    from ortools.linear_solver import pywraplp
-    # gets the variables of the model
-    lp_vars = lp.variables()
-    # get var coefficients in obj function
-    obj_coeff = {i:lp.Objective().GetCoefficient(i) for i in lp_vars}
-    lp.Objective().Clear()
-    # add the same obj func
-    lp.Objective().SetMaximization()
-    for i in lp_vars:
-        lp.Objective().SetCoefficient(i, obj_coeff[i])
+    return (bar_A, [(i.to_dict()['name'],round(value(i),5)) for i in basic_vars],
+           [int(str(k)[1:]) for k in lp_vars if round(k.to_dict()['dj'],5) != 0])
