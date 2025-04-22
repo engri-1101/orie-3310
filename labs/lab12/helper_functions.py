c7f89c1af24aeb63b301c3b6d18a4515c3c53f43
Jacob Dentes <jdentes10@gmail.com>
2025-04-21 20:38:35 -0400

    Updated lab12 helper functions

--- labs/lab12/helper_functions.py
+++ labs/lab12/helper_functions.py
@@ -6,9 +6,12 @@ from pulp import *
 def simplex_tableau(lp):
     # gets the variables of the model
     lp_vars = lp.variables()
+    lp_vars = sorted(lp_vars, key=lambda x: int(x.to_dict()['name'][1:]))
     num_basic = len(lp.constraints)
     # checks reduced costs to find basic variables
-    basic_vars = sorted([k for k in lp_vars], key=lambda x: abs(x.to_dict()['dj']))[:num_basic]
+    basic_vars = sorted(lp_vars, key=lambda x: abs(x.to_dict()['dj']))[:num_basic]
+    basic_varnames = set([i.to_dict()['name'] for i in basic_vars])
+    basic_vars = [k for k in lp_vars if k.to_dict()['name'] in basic_varnames]
     # basic_vars = [k for k in lp_vars if round(k.to_dict()['dj'],5) == 0][:num_basic]
     def get_coeff(cons, var):
         d = cons[1]
@@ -19,7 +22,6 @@ def simplex_tableau(lp):
     # A is the matrix with the coefficients of the constraints
     A = np.array([[get_coeff(c, i) for i in lp_vars] for c in lp.constraints.items()])
     # B is A limited on columns of basic variables
-    basic_varnames = set([i.to_dict()['name'] for i in basic_vars])
     B = A[:,[k for k in range(len(lp_vars)) if lp_vars[k].to_dict()['name'] in basic_varnames]]
     bar_A =  np.round(np.dot(np.linalg.inv(B),A),5)
     temp = [[basic_vars[i].to_dict()['name']]+bar_A.tolist()[i]+[round(value(basic_vars[i]),5)] 
